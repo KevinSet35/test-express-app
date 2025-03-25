91485a91080140d98bc8a84b3c38677cba727770
Kevin Setayesh <kevinsetayesh7@gmail.com>
2025-03-25 17:50:42 -0400

    Added express server

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hello World!');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware (optional)
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello, world! 🌍');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
